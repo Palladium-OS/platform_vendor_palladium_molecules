macka69/platform_vendor_overlays

overlays: Add Custom font package
Adin's edit: User curated font selection!

Change-Id: I1440d6c51ee25c7e465e83b2f4ec5099f03e741d
Signed-off-by: Jackeagle <jackeagle102@gmail.com>

prebuilt/custom_fonts/Android.mk
@@ -1,0 +1,11 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_PACKAGE_NAME := Custom-Fonts
+LOCAL_SDK_VERSION := current
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := false
+
+include $(BUILD_PACKAGE)

Palladium-OS/platform_vendor_palladium_molecules

prebuilt/custom_fonts/Android.mk
@@ -1,0 +1,11 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_PACKAGE_NAME := Palladium-Fonts
+LOCAL_SDK_VERSION := current
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := false
+
+include $(BUILD_PACKAGE)